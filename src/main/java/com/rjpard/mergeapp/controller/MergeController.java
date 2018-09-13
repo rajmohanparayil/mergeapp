7d9bcde04830f1715310bd035a17a8ec90dbe148
rajmohanparayil <rjpatech@gmail.com>
2018-09-13 12:19:19 -0500

    'addedteam'

--- src/main/java/com/rjpard/mergeapp/controller/MergeController.java
+++ src/main/java/com/rjpard/mergeapp/controller/MergeController.java
@@ -1,10 +1,14 @@
 package com.rjpard.mergeapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rjpard.mergeapp.domain.Message;
+import com.rjpard.mergeapp.domain.Team;
 
 @RestController
 public class MergeController {
@@ -13,5 +17,13 @@ public class MergeController {
 	public Message helloWorld(@RequestParam(value = "message", defaultValue = "HelloWorld") String message) {
 		Message messageObj = new Message(message);
 		return messageObj;
+	}
+
+	@GetMapping("/team")
+	public List<Team> teams() {
+		Team team = new Team("Real Madrid");
+		List<Team> teams = new ArrayList<Team>();
+		teams.add(team);
+		return teams;
 	}
 }
